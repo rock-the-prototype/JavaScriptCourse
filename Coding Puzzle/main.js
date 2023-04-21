767b057f316c36b01e311bed522619b295a32679
Sascha Block <Block@inztitut.de>
2023-04-21 19:13:02 +0200

    Coding Puzzle

--- Coding Puzzle/main.js
+++ Coding Puzzle/main.js
@@ -11,4 +11,33 @@ $(document).ready(function() {
         };
     }
 
+    function convertBinaryToAscii(binaryString) {
+        let asciiString = '';
+        // Schleife durch die Binärzeichenkette
+        for (let i = 0; i < binaryString.length; i += 8) {
+            // Teile die Binärzeichenkette in 8-Bit-Chunks auf
+            const chunk = binaryString.substr(i, 8);
+            // Wandele jeden 8-Bit-Chunk in ASCII-Code um
+            const charCode = parseInt(chunk, 2);
+            // Konvertiere ASCII-Code in Zeichen und füge es zur Ausgabezeichenkette hinzu
+            asciiString += String.fromCharCode(charCode);
+        }
+        return asciiString;
+    }
+
+    function convertBinaryToUnicode(binaryString) {
+        let unicodeString = '';
+        // Schleife durch die Binärzeichenkette
+        for (let i = 0; i < binaryString.length; i += 16) {
+            // Teile die Binärzeichenkette in 16-Bit-Chunks auf
+            const chunk = binaryString.substr(i, 16);
+            // Wandele jeden 16-Bit-Chunk in Unicode-Code um
+            const charCode = parseInt(chunk, 2);
+            // Konvertiere Unicode-Code in Zeichen und füge es zur Ausgabezeichenkette hinzu
+            unicodeString += String.fromCharCode(charCode);
+        }
+        return unicodeString;
+    }
+
+    $('#fileInput').change(handleFileSelect);
 });
