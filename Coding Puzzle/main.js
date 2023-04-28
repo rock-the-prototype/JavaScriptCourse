7a00ca820973fd5f77852f121234ab75f576a0e0
Sascha Block <Block@inztitut.de>
2023-04-28 13:24:10 +0200

    Erweiterung der Codierungsfunktionen

--- Coding Puzzle/main.js
+++ Coding Puzzle/main.js
@@ -1,9 +1,10 @@
 function handleFileSelect(event) {
     const file = event.target.files[0];
     const reader = new FileReader();
-    reader.readAsBinaryString(file);
     reader.onload = function() {
-        const binaryString = reader.result.replace(/\s/g, ''); // entferne alle Leerzeichen
+        const arrayBuffer = reader.result;
+        const uint8Array = new Uint8Array(arrayBuffer);
+        const binaryString = arrayBufferToBinaryString(uint8Array);
         const asciiString = convertBinaryToAscii(binaryString);
         const unicodeString = convertBinaryToUnicode(binaryString, 'UTF-8'); // Umwandlung in UTF-8
         const isoString = convertBinaryToUnicode(binaryString, 'ISO-8859-1'); // Umwandlung in ISO-8859-1
@@ -14,9 +15,21 @@ function handleFileSelect(event) {
         console.log('ISO-8859-1: ' + isoString);
 
         const output = document.getElementById('output');
-        output.innerHTML = `ASCII: ${decodeURIComponent(escape(asciiString))}<br>Unicode: ${decodeURIComponent(escape(unicodeString))}<br>ISO-8859-1: ${decodeURIComponent(escape(isoString))}`;
+        output.innerHTML = `ASCII: ${htmlEncode(asciiString)}<br>Unicode: ${htmlEncode(unicodeString)}<br>ISO-8859-1: ${htmlEncode(isoString)}`;
     };
+
+    reader.readAsArrayBuffer(file);
 }
+
+document.getElementById('fileInput').addEventListener('change', handleFileSelect);
+
+// Funktion zur HTML-Kodierung von Zeichen
+function htmlEncode(str) {
+    return str.replace(/[\u00A0-\u9999<>\&]/gim, function(i) {
+        return '&#' + i.charCodeAt(0) + ';';
+    });
+}
+
 
 document.getElementById('fileInput').addEventListener('change', handleFileSelect);
 
@@ -28,7 +41,7 @@ function convertBinaryToUnicode(binaryString, charset) {
     for (let i = 0; i < binaryString.length; i += 8) {
 
         // Wenn das nächste Zeichen ein 16-Bit-Zeichen ist, verarbeite es als solches
-        if (binaryString.charAt(i) === '1' && binaryString.charAt(i+1) === '0') {
+        if (binaryString.charAt(i) === '1' && binaryString.charAt(i + 1) === '0') {
             const chunk = binaryString.substr(i, 16);
             charCode = parseInt(chunk, 2);
             i += 8; // überspringe das zweite Byte des 16-Bit-Zeichens
@@ -53,8 +66,9 @@ function convertBinaryToUnicode(binaryString, charset) {
                 byte1 = 224 + (charCode >> 12);
                 byte2 = 128 + ((charCode >> 6) & 63);
                 byte3 = 128 + (charCode & 63);
-                unicodeString += String.fromCharCode(byte1, byte2, byte3)} else {
-// 4 Bytes
+                unicodeString += String.fromCharCode(byte1, byte2, byte3);
+            } else {
+                // 4 Bytes
                 byte1 = 240 + (charCode >> 18);
                 byte2 = 128 + ((charCode >> 12) & 63);
                 byte3 = 128 + ((charCode >> 6) & 63);
@@ -64,19 +78,8 @@ function convertBinaryToUnicode(binaryString, charset) {
         } else if (charset === 'ISO-8859-1') {
             unicodeString += String.fromCharCode(charCode);
         } else {
-// Standardmäßig wird UTF-8 verwendet
-            if (charCode < 128) {
-                unicodeString += String.fromCharCode(charCode);
-            } else if (charCode < 256) {
-                byte1 = 194;
-                byte2 = charCode;
-                unicodeString += String.fromCharCode(byte1, byte2);
-            } else {
-                byte1 = 224 + (charCode >> 12);
-                byte2 = 128 + ((charCode >> 6) & 63);
-                byte3 = 128 + (charCode & 63);
-                unicodeString += String.fromCharCode(byte1, byte2, byte3);
-            }
+            // Standardmäßig wird UTF-8 verwendet
+            unicodeString += String.fromCharCode(charCode);
         }
     }
     return unicodeString;
@@ -97,3 +100,19 @@ function convertBinaryToAscii(binaryString) {
     return asciiString;
 }
 
+function arrayBufferToBinaryString(arrayBuffer) {
+    const byteArray = new Uint8Array(arrayBuffer);
+    let binaryString = '';
+    for (let i = 0; i < byteArray.byteLength; i++) {
+        binaryString += String.fromCharCode(byteArray[i]);
+    }
+    return binaryString;
+}
+
+function convertUint8ArrayToBinaryString(uint8Array) {
+    let binaryString = '';
+    for (let i = 0; i < uint8Array.length; i++) {
+        binaryString += String.fromCharCode(uint8Array[i]);
+    }
+    return decodeURIComponent(escape(binaryString));
+}
